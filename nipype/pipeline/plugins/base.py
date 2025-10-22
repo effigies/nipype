72aad243042cad0cc11df7cda0433223555ddec2
Christopher J. Markiewicz <markiewicz@stanford.edu>
2025-10-22 06:53:56 +0200

    fix: Use shlex.join to generate SGE batch file

--- nipype/pipeline/plugins/base.py
+++ nipype/pipeline/plugins/base.py
@@ -5,6 +5,7 @@ import sys
 from copy import deepcopy
 from glob import glob
 import os
+import shlex
 import shutil
 from time import sleep, time
 from traceback import format_exception
@@ -564,7 +565,7 @@ class SGELikeBatchManagerBase(DistributedPluginBase):
         batch_dir, name = os.path.split(pyscript)
         name = ".".join(name.split(".")[:-1])
         batchscript = "\n".join(
-            (self._template.rstrip("\n"), f"{sys.executable} {pyscript}")
+            (self._template.rstrip("\n"), shlex.join([sys.executable, pyscript]))
         )
         batchscriptfile = os.path.join(batch_dir, "batchscript_%s.sh" % name)
         with open(batchscriptfile, "w") as fp:
